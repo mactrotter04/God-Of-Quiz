cb24cd7ff3ea8e83d7ba9326103a9ab2cf611129
mactrotter04 <mactrotter04@gmail.com>
2026-06-24 20:49:11 +0100

    added main menu and buttons
    buttons aren't interactive at the moment so need to fix

---
+++ Assets/Scripts/Scene handler.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public void ReloadLevel()
+    {
+
+        Time.timeScale = 1f; //starts time 
+        Cursor.lockState = CursorLockMode.Locked; //the cursor is invisible in the lock state 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR //exists out of the unity game window
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif // exits game
+
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            Debug.Log("no more Scenes to load");
+            SceneManager.LoadScene("MainMenu");
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+}
+
+
